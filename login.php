505c96f5c3c57ad7ac41a3c53886f853e34026ec
Thejaswin S <52855622+thejaswin123@users.noreply.github.com>
2022-02-13 16:40:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -67,7 +67,7 @@ $conn = mysqli_connect("localhost","root","","hospital");
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>apcAKare</title>
+  <title></title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
